e3ae4d9cb11020b9089dceec358911fdde8c0277
Athena Braun <eabraun96@gmail.com>
2024-02-20 18:41:45 -0500

    mostly done solution

--- week3/school/students/admin.py
+++ week3/school/students/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Student
+from .models import *
 
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "age", "eye_color")
 
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "subject", "duration")
+
 # Register your models here.
 admin.site.register(Student, StudentAdmin)
+admin.site.register(Class, ClassAdmin)
--- week3/school/students/views.py
+++ week3/school/students/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
 def index(request):
-    return render(request, "students/index.html")
+    return render(request, "students/index.html", {
+        "total": Student.objects.count(),
+        "students": Student.objects.all()
+    })
+
+# def enroll(request, student_id):
+#     return render(request, "students/enroll.html", {
+#         "student_id": student_id
+#     })
