143dc4f6091dfb8fcbfec4d1faf2581087530257
Athena Braun <eabraun96@gmail.com>
2024-02-20 15:04:15 -0500

    initial student model

--- week3/school/students/admin.py
+++ week3/school/students/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Student
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "age", "eye_color")
+
 # Register your models here.
+admin.site.register(Student, StudentAdmin)
