f25ddf592578640ed181dbd8f270e7187b97e3ec
Athena Braun <eabraun96@gmail.com>
2024-03-04 18:26:42 -0500

    finished displaying info

--- week4/adoption/pets/static/scripts/script.js
+++ week4/adoption/pets/static/scripts/script.js
@@ -1,6 +1,29 @@
 function showPetInfo(petId) {
     // open whatever element on the html will exist, and then put the info in
     console.log("clicked on ", petId);
+    divElem = document.getElementById("pet_info");
+
+    fetch(`info/${petId}`)
+    .then(response => response.json())
+    .then(pet => {
+        pet = pet.pet
+        divElem.innerHTML= `
+            <div>
+                <h4>${pet.name}</h4>
+                <div>
+                    <p>Species: ${pet.species}</p>
+                    <p>Age: ${pet.age + pet.age_unit}</p>
+                    <p>Gender: ${pet.gender}</p>
+                </div>
+                <div>
+                    <p>${pet.description}<p>
+
+                </div>
+            </div>
+        `;
+        console.log(divElem.innerHTML)
+    })
+    .catch(error => console.log(error));
 }
 
 function loadPets() {
@@ -13,32 +36,20 @@ function loadPets() {
         pets = pets.pets
 
         pets.forEach(pet => {
-            // <div class="card">
-            //     <div class="card-body">
-            //         <h5 class="card-title">Koda</h5>
-            //         <p class="card-text">Dog, 5 yr old</p>
-            //     </div>
-            // </div>
-            
             // create our divs etc
             const card = document.createElement("div");
             card.classList.add("card");
+
+            // set the html the user will see
+            // Sneak peek: this is gross! React will make it nicer :)
             card.innerHTML = 
             `<div class="card-body">
                 <h5 class="card-title">${pet.name}</h5>
                 <p class="card-text">${pet.species}, ${pet.age}${pet.age_unit}</p>
             </div>`
 
+            // set the data-ETC property so we can leverage it in the UI
             card.dataset.petId = pet.id;
-
-            // const li = document.createElement("li");
-            
-            // li.id = pet.id;
-            // // set the html the user will see
-            // li.innerHTML = `${pet.name} (${pet.species}, ${pet.age}${pet.age_unit})`
-            
-            // // set the data-ETC property so we can leverage it in the UI
-            // li.dataset.petId = pet.id
 
             // add an event listener
             // note that arrow functions do not have their own 'this' context. If you want to use an arrow function, 
@@ -47,11 +58,12 @@ function loadPets() {
                 // unhighlight any other element
                 document.querySelectorAll(".card").forEach(item => {
                     if (item !== this) {
-                        item.style.backgroundColor = ""
+                        item.style.backgroundColor = "";
                     }
                 });
                 
                 const highlightColor = "lightblue";
+
                 // toggle highlight on this clicked-on element
                 if (this.style.backgroundColor === highlightColor) {
                     this.style.backgroundColor = "";
@@ -65,7 +77,8 @@ function loadPets() {
 
             availablePetsHtml.append(card);
         });
-    });
+    })
+    .catch(error => console.log(error));
 }
 
 document.addEventListener("DOMContentLoaded", () => {
